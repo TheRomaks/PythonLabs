f19ef31cb3c435ff4a0aac7a7d6297549f32fd2f
TheRomaks <niks_2004@bk.ru>
2024-09-23 17:06:59 +0300

    all labs

---
+++ 08_garden.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# в саду сорвали цветы
+garden = ('ромашка', 'роза', 'одуванчик', 'ромашка', 'гладиолус', 'подсолнух', 'роза', )
+
+# на лугу сорвали цветы
+meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер', 'мак', 'одуванчик', 'ромашка', )
+
+# создайте множество цветов, произрастающих в саду и на лугу
+garden_set = set(garden)
+meadow_set = set(meadow)
+# TODO здесь ваш код
+
+# выведите на консоль все виды цветов
+# TODO здесь ваш код
+all_flowers = garden_set.union(meadow_set)
+print("Все виды цветов:", all_flowers)
+
+# выведите на консоль те, которые растут и там и там
+# TODO здесь ваш код
+common_flowers = garden_set.intersection(meadow_set)
+print("Цветы, растущие и там, и там:", common_flowers)
+
+# выведите на консоль те, которые растут в саду, но не растут на лугу
+# TODO здесь ваш код
+garden_only = garden_set.difference(meadow_set)
+print("Цветы, растущие только в саду:", garden_only)
+
+# выведите на консоль те, которые растут на лугу, но не растут в саду
+# TODO здесь ваш код
+meadow_only = meadow_set.difference(garden_set)
+print("Цветы, растущие только на лугу:", meadow_only)
